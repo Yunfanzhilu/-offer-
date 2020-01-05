e7d529df0a2230c00a2c209fcc8b4cfd8ea09d43
Tony Huang <42203111+Yunfanzhilu@users.noreply.github.com>
2020-01-05 10:45:36 +0800

    Update 用两个栈实现队列.cpp
    以下我自己想的代码，可能还有些错误，后面改正再看

--- 用两个栈实现队列.cpp
+++ 用两个栈实现队列.cpp
@@ -1,6 +1,7 @@
 题目内容：用两个栈实现一个队列，完成队列的Push和Pop操作。队列中的元素为int类型
 
 算法：栈是先进后出的结构，队列是先进先出的结构，简单的设想一组int类型的数据[1,2,3,4,5]，如果先进入第一个栈,那么第一个栈的内容（从栈顶到栈底）：5,4,3,2,1，再进入第二个栈，第二个栈的内容（从栈顶到栈底）：1,2,3,4,5，现在再出栈的话不就和队列的性质的一样了，如果中途还想有栈加入，那也是一样的道理。下面是牛客网通过代码：
+//牛客网经典代码
 class Solution
 {
 public:
@@ -70,10 +71,7 @@ push:将一个元素置入queue中
 
 - 调用者保证queue非空
 
-## 代码
-// A queue consisting of two stacks.cpp : 定义控制台应用程序的入口点。
-//
-
+## 他人代码（自己稍加改动）
 #include "stdafx.h"
 #include<iostream>
 #include<stack>
@@ -163,4 +161,100 @@ int _tmain(int argc, _TCHAR* argv[])
 }
 
 
+//以下我自己想的代码，可能还有些错误，后面改正再看
+#include "stdafx.h"
+#include<iostream>
+#include<stack>
+#include<queue>
+using namespace std;
+class MyTwoStackToQueue
+{
+public:
+	void push(int newnum);
+	void pop();
+	void adjust();
+	int front();
+	int back();
+private:
+	int tempnum;
+	stack<int>pushstack;
+	stack<int>popstack;
+};
+void MyTwoStackToQueue::adjust()
+{
+	while (!pushstack.empty())
+	{
+		popstack.push(pushstack.top());
+		pushstack.pop();
+	}
+}
+void MyTwoStackToQueue::push(int newnum)
+{
+	
+	tempnum = newnum;
+	pushstack.push(newnum);
 
+}
+void MyTwoStackToQueue::pop()
+{
+	
+	popstack.pop();
+}
+int MyTwoStackToQueue::front()
+{
+	if ((!pushstack.empty())&&(popstack.empty()))
+	{
+		adjust();
+	}
+	return popstack.top();
+
+}
+int MyTwoStackToQueue::back()
+{
+	if (popstack.empty())
+	{
+		adjust();
+	}
+	else
+	{
+		if (!pushstack.empty())
+		{
+			return pushstack.top();
+		}
+		else
+		{
+			return tempnum;
+		}
+	}
+
+}
+int main()
+{
+	int i, j;
+	int a1 = 92;
+	int a2 = 92;
+	MyTwoStackToQueue newqueue;
+	cout << "循环添加元素的操作：" << endl;
+	for (i = 0; i < 10; i++)
+	{
+		newqueue.push(i);
+
+	}
+	cout << newqueue.front() << "应:0," << endl;
+	cout<<newqueue.back() << "应:9," << endl;
+	cout << "添加单个元素10的操作：" << endl;
+	newqueue.push(10);
+	cout << newqueue.front() << "应:0," << endl;
+	cout<< newqueue.back() << "应:10" << endl;
+	newqueue.push(11);
+	cout << newqueue.front() << "应:0," << endl;
+	cout << newqueue.back() << "应:11" << endl;
+	newqueue.push(12);
+	cout << newqueue.front() << "应:0," << endl;
+	cout << newqueue.back() << "应:12" << endl;
+	cout << "删除单个元素的操作：" << endl;
+newqueue.pop();
+cout << newqueue.front() << "应:1," << endl;
+cout<<newqueue.back() << "应:12" << endl;
+
+}
